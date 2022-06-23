99f18dd1cbc12ffaf20e59d21fa7295852f987ea
andrevcaseiro <andre.caseir02@gmail.com>
2022-06-23 17:33:21 +0100

    Remove unrequested prints

--- takuzu.py
+++ takuzu.py
@@ -253,5 +253,5 @@ if __name__ == "__main__":
     # Obter o nó solução usando a procura em profundidade:
     goal_node = depth_first_tree_search(problem)
     # Verificar se foi atingida a solução
-    print("Is goal?", problem.goal_test(goal_node.state))
-    print("Solution:\n", goal_node.state.board, sep="")
+    # print("Is goal?", problem.goal_test(goal_node.state))
+    print(goal_node.state.board, end="")
